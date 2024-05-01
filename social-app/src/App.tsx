256a746cab875410329d0fdbc7259bc9e79ad7c7
josephcoste <josephcoste0@gmail.com>
2024-05-01 16:57:16 -0400

    updated code

--- social-app/src/App.tsx
+++ social-app/src/App.tsx
@@ -3,17 +3,25 @@ import {Routes , Route } from 'react-router-dom';
 import './globals.css';
 import SigninForm from './_auth/forms/SigninForm';
 import { Home } from './_root/pages'
+import AuthLayout from './_auth/forms/AuthLayout';
+import RootLayout from './_root/pages/RootLayout';
+import SignupForm from './_auth/forms/SignupForm';
 
 const App = () => {
   return (
     <main className = "flex h-screen">
 <Routes>
   { }  
+  <Route element={<AuthLayout/>}>
 <Route path="/sign-in" element={<SigninForm/>}/>
-<Route path="/sign-in" element={<SigninForm/>}/>
+<Route path="/sign-up" element={<SignupForm/>}/>
+</Route>
   { }
+  <Route element={<RootLayout/>}>
   <Route index element={<Home/>} />
+  </Route>
  </Routes>
+
 </main>
   )
 }
